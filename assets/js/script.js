74b54afba78f4ad3dc1ce2e0fee78cdc41a59501
Kasper (git commits by Albert) <54741819+kasperireland@users.noreply.github.com>
2022-02-19 20:25:58 -0500

    add space (make more readable)

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ window.onload = function WindowLoad(event) {
 var searchValue = document.getElementById("searchValue");
 searchValue.placeholder = "Search via " + localStorage.getItem("searchname"); 
 var engine = document.getElementById("engines");
+    
 if (localStorage.getItem("searchengine")) {
     engine.value = (localStorage.getItem("searchname"));
     
