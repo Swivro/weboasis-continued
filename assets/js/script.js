ea69bc3d4c10ebe6104c762408904fe4c4af5fae
Kasper (git commits by Albert) <54741819+kasperireland@users.noreply.github.com>
2022-02-19 20:01:43 -0500

    set search box placeholder on page load

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 window.onload = function WindowLoad(event) {
+var searchValue = document.getElementById("searchValue");
+searchValue.placeholder = "Search via " + localStorage.getItem("searchname"); 
 var engine = document.getElementById("engines");
 if (localStorage.getItem("searchengine")) {
     engine.value = (localStorage.getItem("searchname"));
